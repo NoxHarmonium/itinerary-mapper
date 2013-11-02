8a3e4e5a08a9a5857dafcb5b074919ac8661d162
Sean Dawson <sean@two-bulls.com>
2013-11-03 10:15:40 +1100

    Itinerary points are now mapped and the current stage of the itinerary is shown in the title bar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-var main = (function ($, google) {
+var main = (function ($, google, Date) {
   "use strict";
   var itineraryData, methods, markers;
   var gm = google.maps;
   var map;
+  var currentMarker;
 
   var jqxhr = $.getJSON( "data/itinerary.json" )
   .done(function(result) {
       itineraryData = result;
       methods.init();
       methods.zoomToMarkers();
+      methods.setCurrentActivityForDate(Date.today());
+      $('#datepicker').datepicker();
+
   })
   .fail(function(error) {
     console.log( "Could not load itinerary data: " + error.statusText );
@@ -29,6 +33,12 @@ var main = (function ($, google) {
       map = new google.maps.Map(document.getElementById("map-canvas"),
           mapOptions);
 
+       // Define a symbol using a predefined path (an arrow)
+      // supplied by the Google Maps JavaScript API.
+      var lineSymbol = {
+        path: gm.SymbolPath.FORWARD_CLOSED_ARROW
+      };
+
       var points = itineraryData.points;
 
       if (!points)
@@ -38,6 +48,8 @@ var main = (function ($, google) {
       }
 
       var point, loc, loc_, marker, isLastPoint, destPoint, line, loc2, transitType, icon;
+      
+
       for(i = 0; i < points.length; i++)
       {
         point = points[i];
@@ -72,7 +84,14 @@ var main = (function ($, google) {
             path: [
               new gm.LatLng(loc.latitude, loc.longitude),
               new gm.LatLng(loc2.latitude, loc2.longitude) 
-            ]
+            ],
+            icons: [{
+              icon: lineSymbol,
+              offset: '100%'
+            }],
+            strokeWeight: 1.5,
+            strokeColor: "#555555"
+
           });
         }
       }
@@ -86,9 +105,54 @@ var main = (function ($, google) {
       }
       map.fitBounds (bounds);
 
+    },
+    "setCurrentActivityForDate": function(date)
+    {
+      var point, pointDate;
+      var points = itineraryData.points;
+      var currentIndex = points.length - 1;
+      for(var i = 0; i < points.length; i++)
+      {
+        
+        point = points[i];
+        pointDate = Date.parse(point.datetime);
+        //console.log(pointDate + ">" + date);
+        if (pointDate.isAfter(date))
+        {
+          currentIndex = i-1;
+          break;
+        }
+      }
+
+      var text = "";
+      if (currentIndex < 0)
+      {
+        text = "Current Activity: " + " Waiting to start";
+        point = points[0];
+      }
+      else
+      {
+        text = "Current Activity: " + points[currentIndex].title;
+        point = points[currentIndex];
+      }
+      
+      $("#activity-bar>span.link-text").text(text);
+      $("#current-time").text(date.toString('dddd, MMMM d, yyyy'));
+
+      var loc = point.location;
+      var loc_ = new gm.LatLng(loc.latitude, loc.longitude);
+
+      if (currentMarker)
+      {
+        currentMarker.setMap(null);
+      }
+
+      currentMarker = new gm.Marker({
+            map: map,
+            position: loc_,
+          });    
+
     }
-
-
 
   }
 
@@ -101,6 +165,6 @@ var main = (function ($, google) {
 
 google.maps.event.addDomListener(window, 'load', function()
   {
-    main($, google);
+    main($, google, Date);
   }
 );
